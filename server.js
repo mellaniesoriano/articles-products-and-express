9d9e618b9970afddf2b17cab3e4b97dfa4991c87
mellaniesoriano <msoriano808@gmail.com>
2017-07-15 18:21:25 -1000

    server connecting

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 /* jshint esversion: 6 */
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const articlesRoute = require('./routes/articles.js');
+const productsRoute = require('./routes/products.js');
+
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
