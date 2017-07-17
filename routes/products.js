028a2ddbb4c13a92faf9bade86e0eafc3986036f
mellaniesoriano <msoriano808@gmail.com>
2017-07-16 22:46:09 -1000

    products routing correctly

--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,16 @@
 /* jshint esversion: 6 */
-
 const express = require('express');
 const router = express.Router();
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const productsDB = require('../db/productsDB.js');
+
+router.route('/')
+.get((req, res) => {
+  res.render('index', {catalog: productsDB.getProduct()});
+})
+.post((req, res) => {
+  checkProperties(req, res);
+});
+
+module.exports = router;
