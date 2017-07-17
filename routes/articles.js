25e9962a991e66197f7aba66569f9fdcc9383099
mellaniesoriano <msoriano808@gmail.com>
2017-07-16 22:45:32 -1000

    initial setup

--- routes/articles.js
+++ routes/articles.js
@@ -1,4 +1,8 @@
 /* jshint esversion: 6 */
-
 const express = require('express');
 const router = express.Router();
+const exphbs = require('express-handlebars');
+
+router.route('/');
+
+module.exports = router;
